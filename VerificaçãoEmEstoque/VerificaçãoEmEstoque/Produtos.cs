c10c4bf2098d598617879dd55378e1fcba06ca9c
caiocezar <caiocezar.bezerra@hotmail.com>
2024-10-20 14:35:39 -0300

    Estoque

---
+++ VerificaçãoEmEstoque/VerificaçãoEmEstoque/Produtos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace VerificaçãoEmEstoque
+{
+    internal class Produtos
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdiocionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Preco.ToString("F2", CultureInfo.InvariantCulture) + " , " + Quantidade + " Unidades, Total $" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ VerificaçãoEmEstoque/VerificaçãoEmEstoque/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Globalization;
+using VerificaçãoEmEstoque;
+
+Produtos p = new Produtos();
+
+Console.WriteLine("Entre os dados do produto:");
+Console.Write("Nome: ");
+p.Nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantidade: ");
+p.Quantidade = int.Parse(Console.ReadLine());
+Console.WriteLine("Dados do produto: " + p);
+
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos a ser adicionado no estoque: ");
+int qte = int.Parse(Console.ReadLine());
+p.AdiocionarProdutos(qte);
+Console.WriteLine();
+Console.WriteLine("Dados Atulizados: " + p);
+
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos a ser Removido do estoque: ");
+qte = int.Parse(Console.ReadLine());
+p.RemoverProdutos(qte);
+Console.WriteLine();
+Console.WriteLine("Dados Atulizados: " + p);
